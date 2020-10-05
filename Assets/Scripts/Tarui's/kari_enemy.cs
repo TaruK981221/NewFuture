e78d058ea6e6e20f7e323e7a9ce0805e25402238
TaruK981221 <tarui2412git@gmail.com>
2020-10-05 12:41:05 +0900

    仮のプレハブ作成 EnemyScene作成 仮の敵用スクリプト作成

---
+++ Assets/Scripts/Tarui's/kari_enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kari_enemy : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigidbody.AddForce(new Vector2(1.0f, 0.0f), ForceMode2D.Force);
+    }
+}
