8655c3ffb831ccf3a066c0de87b5c0cac96cbd21
jillsher <jillsher360@gmail.com>
2020-10-09 12:58:51 +0900

    ステージ（壁・床・ブロック・上下床）追加

---
+++ Assets/Scripts/id/MoveBlock.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBlock : MonoBehaviour
+{
+
+    private Rigidbody2D rigid;
+    private Vector3 defaultPos;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        defaultPos = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        rigid.MovePosition(new Vector2(defaultPos.x, defaultPos.y + Mathf.PingPong(Time.time, 2), defaultPos.z));
+    }
+}
