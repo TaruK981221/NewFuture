7b05cdb8866fc46d2f7890f5df1640801e60917d
umigame0 <umigame00@gmail.com>
2020-10-13 22:12:02 +0900

    追従カメラ・背景の実装

---
+++ Assets/Scripts/Fujita's/TemporaryPlayerMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[DefaultExecutionOrder(0)]
+
+public class TemporaryPlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*---- 自オブジェクト移動処理(簡易) ----*/
+        Vector3 pos = this.transform.position;      // インスタンス生成
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            pos.x += 0.1f;
+            this.transform.position = pos;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            pos.x -= 0.1f;
+            this.transform.position = pos;
+        }
+    }
+}
