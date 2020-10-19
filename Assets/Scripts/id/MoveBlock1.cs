c884cb58738ab64b8e1aeba9dff363f5dfbefbad
jillsher <jillsher360@gmail.com>
2020-10-19 15:09:40 +0900

    追加物　　判定床

---
+++ Assets/Scripts/id/MoveBlock1.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBlock1 : MonoBehaviour
+{
+
+    private Rigidbody2D rigid;
+    private Vector2 defaultPos;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        defaultPos = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        rigid.MovePosition(new Vector2(defaultPos.x, defaultPos.y + Mathf.PingPong(Time.time, 2)));
+    }
+}
