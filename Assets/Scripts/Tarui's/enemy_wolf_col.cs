2b07fa27427e5bdecdcaaa472c6cb41cb460bec4
TaruK981221 <tarui2412git@gmail.com>
2020-11-16 10:21:48 +0900

    画像追加分を移動 敵の動きを改変

---
+++ Assets/Scripts/Tarui's/enemy_wolf_col.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_wolf_col : MonoBehaviour
+{
+    enemy_wolf parentCom = null;
+
+    // 攻撃対象
+    Transform player = null;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        parentCom = this.transform.parent.GetComponentInChildren<enemy_wolf>();
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform == player)
+        {
+            parentCom.AtkCollision();
+        }
+    }
+}
