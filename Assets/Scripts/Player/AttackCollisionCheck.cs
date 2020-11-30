86405594cc98ecd8a7ed909a903f3b1ecf2511eb
TakamasaKoshima <light15_aldebaran@yahoo.co.jp>
2020-11-30 21:57:35 +0900

    ・攻撃判定用プレハブの追加。（サイスとクロースタイルの分） ・プレイヤーの攻撃当たり判定用スクリプト追加。 ・アニメーションにアタッチする用のスクリプト（終了フラグ立てる）追加。

---
+++ Assets/Scripts/Player/AttackCollisionCheck.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// プレイヤーの攻撃エフェクトの当たり判定用処理
+/// </summary>
+public class AttackCollisionCheck : MonoBehaviour
+{
+    private string enemyTag = "enemy";
+    private string breakableTag = "BreakableBlock";
+    private bool isCollision = false;
+    private bool isCollisionEnter, isCollisionStay, isCollisionExit;
+
+
+    //衝突判定を返すメソッド
+    //物理判定の更新毎に呼ぶ必要がある
+    public bool IsCollision()
+    {
+        if (isCollisionEnter || isCollisionStay)
+        {
+            isCollision = true;
+        }
+        else if (isCollisionExit)
+        {
+            isCollision = false;
+        }
+
+        isCollisionEnter = false;
+        isCollisionStay = false;
+        isCollisionExit = false;
+        return isCollision;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == enemyTag)
+        {
+            isCollisionEnter = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == enemyTag)
+        {
+            isCollisionStay = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == enemyTag)
+        {
+            isCollisionExit = true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player/IsAnimationCheck.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsAnimationCheck : MonoBehaviour
+{
+    private bool m_endAnimFlag = false;
+    
+    public bool EndAnimation()
+    {
+        return m_endAnimFlag;
+    }
+
+    private void AnimationEndFlagON()
+    {
+        Debug.Log("AnimationEndFlagON():::"+ m_endAnimFlag);
+        m_endAnimFlag = true;
+    }
+
+    private void AnimationEndFlagOFF()
+    {
+        Debug.Log("AnimationEndFlagOFF():::"+ m_endAnimFlag);
+        m_endAnimFlag = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
