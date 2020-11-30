b210f05eed788dc78bdbfb37c27fa512e16df5f7
TakamasaKoshima <light15_aldebaran@yahoo.co.jp>
2020-11-30 21:54:16 +0900

    ・スクリプトの追加。 ①入力キーの状態（ホールドなど）をフラグとして管理するクラス。 ②プレイヤー用の構造体や列挙体をまとめたもの。

---
+++ Assets/Scripts/Player/InputFlag.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TeamProject
+{
+    /// <summary>
+    /// キー入力時の状態（down,keep,up）を持つためのクラス
+    /// </summary>
+    enum KEYSTATE
+    {
+        DOWN = 0,//押し始めた時
+        HOLD,//押したまま
+        UP,//放した時
+        ALL
+    }
+    public class InputFlag 
+    {
+        public bool[] m_keyFlag = new bool[(int)KEYSTATE.ALL];
+
+        /// <summary>
+        /// フラグを3つともfalseにする
+        /// </summary>
+        public void KeyFlagReset()
+        {
+            for (int i = 0; i < (int)KEYSTATE.ALL; i++)
+            {
+                m_keyFlag[i] = false;
+            }
+        }
+
+        public void KeyFlagCheck(InputFlag _Key, InputManager.ButtonCode _buttonCode)
+        {
+            _Key.m_keyFlag[(int)KEYSTATE.DOWN] = InputManager.InputManager.Instance.GetKeyDown(_buttonCode);
+            _Key.m_keyFlag[(int)KEYSTATE.HOLD] = InputManager.InputManager.Instance.GetKey(_buttonCode);
+            _Key.m_keyFlag[(int)KEYSTATE.UP]   = InputManager.InputManager.Instance.GetKeyUp(_buttonCode);
+        }
+        public void KeyFlagCheck(InputFlag _Key, KeyCode _keyCode)
+        {
+            _Key.m_keyFlag[(int)KEYSTATE.DOWN] = Input.GetKeyDown(_keyCode);
+            _Key.m_keyFlag[(int)KEYSTATE.HOLD] = Input.GetKey(_keyCode);
+            _Key.m_keyFlag[(int)KEYSTATE.UP] = Input.GetKeyUp(_keyCode);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerDefine.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//プレイヤーの列挙体など宣言用スクリプト
+namespace TeamProject
+{
+    //プレイヤーの状態
+    public enum P_STATE
+    {
+        IDLE = 0,                   //待機
+        RUN,                        //走る
+        RISE,                       //上昇
+        FALL,                       //下降
+        ATTACK,                     //攻撃
+        JUMP_ATTACK,                //ジャンプ攻撃
+        DAMAGE,                     //被ダメージ
+        STYLE_CHANGE_NEXT,          //次のスタイルチェンジ
+        STYLE_CHANGE_PREV,          //前のスタイルチェンジ
+        MAX_STATE                   //全状態数
+    }
+    //プレイヤーの方向
+    public enum P_DIRECTION
+    {
+        RIGHT = 0,    //右
+        LEFT,         //左
+        MAX_DIRECT    //全方向
+    }
+    //プレイヤーの接地状態
+    public enum P_GROUND
+    {
+        GROUND = 0,    //接地
+        JUMP_1ST,      //ジャンプ1回目
+        JUMP_2ND,      //ジャンプ2回目
+        FALL,          //落下
+        MAX_STATE      //全状態数
+    }
+
+    public enum P_STYLE
+    {
+        SCYTHE = 0,     //大鎌スタイル（パワー）
+        CLAW,           //鉤爪スタイル（スピード）
+        MAGIC,          //魔法スタイル（マジック）
+        MAX_STYLE
+    }
+
+    public struct P_PARAMETER
+    {
+        public P_STATE m_PlayerState;
+        public P_GROUND m_PlayerGround;
+        public P_DIRECTION m_PlayerDirection;
+        public P_STYLE m_playerStyle;
+
+    }
+
+    
+    //public class PlayerDefine : MonoBehaviour
+    //{
+
+    //}//    public class PlayerDefine : MonoBehaviour END
+}//namespace TeamProject
