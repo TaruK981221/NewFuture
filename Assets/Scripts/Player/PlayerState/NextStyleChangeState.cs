8f0039e3a55bf5cc771680db3dabfb798b306355
TakamasaKoshima <light15_aldebaran@yahoo.co.jp>
2020-11-30 21:59:10 +0900

    ・スタイルチェンジの状態用のスクリプトの追加。

---
+++ Assets/Scripts/Player/PlayerState/NextStyleChangeState.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TeamProject
+{
+
+    public class NextStyleChangeState : PlayerState
+    {
+
+        public NextStyleChangeState()
+        {
+            SetNextState(this);
+            SetPrevState(this);
+        }
+        override public void SetSelfState() { SelfState = P_STATE.STYLE_CHANGE_NEXT; }
+
+        // Update is called once per frame
+        override public bool Update()
+        {
+            //PositionUpdate();
+            if (m_endAnimation)
+            {
+                SetPrevState(this);
+                SetNextState(m_idleState);
+                SetEndAnimFlag(false);
+
+                ChangeNextStyle();               
+                Debug.Log("次通った");
+                return true;
+            }
+            SetNextState(this);
+            SetPrevState(this);
+
+            Debug.Log("次通ってない");
+
+            return false;
+        }
+    }//    public class NextStyleChangeState : PlayerState END
+}//namespace TeamProject END
---
+++ Assets/Scripts/Player/PlayerState/PrevStyleChangeState.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TeamProject
+{
+
+    public class PrevStyleChangeState : PlayerState
+    {
+        public PrevStyleChangeState()
+        {
+            SetNextState(this);
+            SetPrevState(this);
+        }
+        override public void SetSelfState() { SelfState = P_STATE.STYLE_CHANGE_PREV; }
+
+        // Update is called once per frame
+        override public bool Update()
+        {
+            //PositionUpdate();
+
+            if (m_endAnimation)
+            {
+                SetPrevState(this);
+                SetNextState(m_idleState);
+                SetEndAnimFlag(false);
+
+                ChangePrevStyle();
+                Debug.Log("前通った");
+                return true;
+            }
+            Debug.Log("前通ってない");
+
+            return false;
+        }
+    }//    public class PrevStyleChangeState : PlayerState END
+}//namespace TeamProject END
