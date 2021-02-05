40214030b9cf5325239be700818c37f29d00e38e
TakamasaKoshima <light15_aldebaran@yahoo.co.jp>
2021-02-05 11:34:11 +0900

    ・他のメンバーのコミットし忘れ分？キメラの一部スクリプト。

---
+++ Assets/Scripts/Tarui's/Boss/Chimera/Boss_Chimera_Fire.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Chimera_Fire : MonoBehaviour
+{
+    Boss_Chimera parent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = GetComponentInParent<Boss_Chimera>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!parent.IsAtk && 
+            collision.gameObject == parent.gameObject)
+        {
+            parent.AtkCol(2);
+        }
+    }
+}
